642faf1f08040e55836187b2f6e51c1f5d3390f6
doug-martin <doug@dougamartin.com>
2020-05-07 23:13:13 -0500

    example(sequelize): Update to use auto-generated resolvers

--- examples/nest-graphql-sequelize/src/sub-task/sub-task.module.ts
+++ examples/nest-graphql-sequelize/src/sub-task/sub-task.module.ts
@@ -1,10 +1,24 @@
+import { NestjsQueryGraphQLModule } from '@nestjs-query/query-graphql/src';
 import { NestjsQuerySequelizeModule } from '@nestjs-query/query-sequelize';
 import { Module } from '@nestjs/common';
+import { SubTaskDTO } from './dto/sub-task.dto';
+import { CreateSubTaskDTO } from './dto/subtask-input.dto';
+import { SubTaskUpdateDTO } from './dto/subtask-update.dto';
 import { SubTaskEntity } from './sub-task.entity';
-import { SubTaskResolver } from './sub-task.resolver';
 
 @Module({
-  providers: [SubTaskResolver],
-  imports: [NestjsQuerySequelizeModule.forFeature([SubTaskEntity])],
+  imports: [
+    NestjsQueryGraphQLModule.forFeature({
+      imports: [NestjsQuerySequelizeModule.forFeature([SubTaskEntity])],
+      resolvers: [
+        {
+          DTOClass: SubTaskDTO,
+          EntityClass: SubTaskEntity,
+          CreateDTOClass: CreateSubTaskDTO,
+          UpdateDTOClass: SubTaskUpdateDTO,
+        },
+      ],
+    }),
+  ],
 })
 export class SubTaskModule {}
