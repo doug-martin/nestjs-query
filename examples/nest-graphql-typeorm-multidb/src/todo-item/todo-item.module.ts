70a88ab6563bb505d3b15f8001c6d4c72a34551b
doug-martin <doug@dougamartin.com>
2020-05-07 23:04:24 -0500

    example(): Update multi-db example to use auto-resolver

--- examples/nest-graphql-typeorm-multidb/src/todo-item/todo-item.module.ts
+++ examples/nest-graphql-typeorm-multidb/src/todo-item/todo-item.module.ts
@@ -1,10 +1,22 @@
+import { NestjsQueryGraphQLModule } from '@nestjs-query/query-graphql/src';
 import { NestjsQueryTypeOrmModule } from '@nestjs-query/query-typeorm';
 import { Module } from '@nestjs/common';
+import { TodoItemInputDTO } from './dto/todo-item-input.dto';
+import { TodoItemDTO } from './dto/todo-item.dto';
 import { TodoItemEntity } from './todo-item.entity';
-import { TodoItemResolver } from './todo-item.resolver';
 
 @Module({
-  providers: [TodoItemResolver],
-  imports: [NestjsQueryTypeOrmModule.forFeature([TodoItemEntity])],
+  imports: [
+    NestjsQueryGraphQLModule.forFeature({
+      imports: [NestjsQueryTypeOrmModule.forFeature([TodoItemEntity])],
+      resolvers: [
+        {
+          DTOClass: TodoItemDTO,
+          EntityClass: TodoItemEntity,
+          CreateDTOClass: TodoItemInputDTO,
+        },
+      ],
+    }),
+  ],
 })
 export class TodoItemModule {}
--- examples/nest-graphql-typeorm-multidb/src/user/user.module.ts
+++ examples/nest-graphql-typeorm-multidb/src/user/user.module.ts
@@ -1,11 +1,24 @@
+import { NestjsQueryGraphQLModule } from '@nestjs-query/query-graphql/src';
 import { NestjsQueryTypeOrmModule } from '@nestjs-query/query-typeorm';
 import { Module } from '@nestjs/common';
 import { USER_CONNECTION } from '../constants';
+import { UserInputDTO } from './dto/user-input.dto';
+import { UserDTO } from './dto/user.dto';
 import { UserEntity } from './user.entity';
-import { UserResolver } from './user.resolver';
 
 @Module({
-  providers: [UserResolver],
-  imports: [NestjsQueryTypeOrmModule.forFeature([UserEntity], USER_CONNECTION)],
+  imports: [
+    NestjsQueryGraphQLModule.forFeature({
+      imports: [NestjsQueryTypeOrmModule.forFeature([UserEntity], USER_CONNECTION)],
+      resolvers: [
+        {
+          DTOClass: UserDTO,
+          EntityClass: UserEntity,
+          CreateDTOClass: UserInputDTO,
+          UpdateDTOClass: UserInputDTO,
+        },
+      ],
+    }),
+  ],
 })
 export class UserModule {}
