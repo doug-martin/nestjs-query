f3142c40d6faa0cc7a0d52ab5218c5b8f83a5d6e
doug-martin <doug@dougamartin.com>
2020-05-18 16:29:52 -0500

    test(graphql): Fix reference resolver tests

--- packages/query-graphql/__tests__/resolvers/__fixtures__/test-resolver.service.ts
+++ packages/query-graphql/__tests__/resolvers/__fixtures__/test-resolver.service.ts
@@ -3,5 +3,6 @@ import { TestResolverDTO } from './test-resolver.dto';
 
 @QueryService(TestResolverDTO)
 export class TestService extends NoOpQueryService<TestResolverDTO> {
+  // eslint-disable-next-line @typescript-eslint/no-explicit-any
   [x: string]: any;
 }
