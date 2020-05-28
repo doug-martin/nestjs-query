35c67c87e3a1b0eb128b6d3437f355bcda1f0340
doug-martin <doug@dougamartin.com>
2020-05-27 21:20:58 -0500

    test(): Increase jest test timeout

--- jest.config.js
+++ jest.config.js
@@ -1,5 +1,6 @@
 // jest.config.js
 module.exports = {
   projects: ['./jest.unit.config.js', './jest.e2e.config.js'],
+  testTimeout: 10000,
   collectCoverageFrom: ['packages/**/*.ts', '!**/__tests__/**', '!**/dist/**', '!**/node_modules/**'],
 };
