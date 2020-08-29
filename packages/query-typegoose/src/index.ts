187fa697bc10e38f705667cc4d71851a027ffa68
marian2js <marianomdq1@gmail.com>
2020-08-29 17:43:45 -0300

    feat(mongodb): Allow to customize mongoose document options

--- packages/query-typegoose/src/index.ts
+++ packages/query-typegoose/src/index.ts
@@ -1,2 +1,2 @@
-export { TypegooseQueryService } from './services';
+export { TypegooseQueryService, TypegooseQueryServiceOpts } from './services';
 export { NestjsQueryTypegooseModule } from './module';
