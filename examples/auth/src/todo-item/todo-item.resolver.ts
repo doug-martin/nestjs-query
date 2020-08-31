50a983bec5bc200a5b0d67b5b0bb1ae1341a6aa4
doug-martin <doug@dougamartin.com>
2020-08-30 21:43:17 -0500

    example(auth): Add authorizer to plain resolver

--- examples/auth/src/todo-item/todo-item.resolver.ts
+++ examples/auth/src/todo-item/todo-item.resolver.ts
@@ -1,35 +1,52 @@
-import { Filter, InjectAssemblerQueryService, QueryService } from '@nestjs-query/core';
-import { ConnectionType } from '@nestjs-query/query-graphql';
-import { Args, Query, Resolver } from '@nestjs/graphql';
+import { Filter, InjectAssemblerQueryService, mergeFilter, mergeQuery, QueryService } from '@nestjs-query/core';
+import { Authorizer, ConnectionType, InjectAuthorizer } from '@nestjs-query/query-graphql';
+import { Args, Query, Resolver, Context } from '@nestjs/graphql';
+import { UseGuards } from '@nestjs/common';
 import { TodoItemDTO } from './dto/todo-item.dto';
 import { TodoItemAssembler } from './todo-item.assembler';
 import { TodoItemConnection, TodoItemQuery } from './types';
+import { UserContext } from '../auth/auth.interfaces';
+import { JwtAuthGuard } from '../auth/jwt-auth.guard';
 
 @Resolver(() => TodoItemDTO)
+@UseGuards(JwtAuthGuard)
 export class TodoItemResolver {
-  constructor(@InjectAssemblerQueryService(TodoItemAssembler) readonly service: QueryService<TodoItemDTO>) {}
+  constructor(
+    @InjectAssemblerQueryService(TodoItemAssembler) readonly service: QueryService<TodoItemDTO>,
+    @InjectAuthorizer(TodoItemDTO) readonly authorizer: Authorizer<TodoItemDTO>,
+  ) {}
 
   // Set the return type to the TodoItemConnection
   @Query(() => TodoItemConnection)
-  completedTodoItems(@Args() query: TodoItemQuery): Promise<ConnectionType<TodoItemDTO>> {
+  async completedTodoItems(
+    @Args() query: TodoItemQuery,
+    @Context() context: UserContext,
+  ): Promise<ConnectionType<TodoItemDTO>> {
+    const authFilter = await this.authorizer.authorize(context);
     // add the completed filter the user provided filter
-    const filter: Filter<TodoItemDTO> = {
-      ...query.filter,
-      ...{ completed: { is: true } },
-    };
-
-    return TodoItemConnection.createFromPromise((q) => this.service.query(q), { ...query, ...{ filter } });
+    const filter: Filter<TodoItemDTO> = mergeFilter(query.filter ?? {}, { completed: { is: true } });
+    const completedQuery = mergeQuery(query, { filter: mergeFilter(filter, authFilter) });
+    return TodoItemConnection.createFromPromise(
+      (q) => this.service.query(q),
+      completedQuery,
+      (q) => this.service.count(q),
+    );
   }
 
   // Set the return type to the TodoItemConnection
   @Query(() => TodoItemConnection)
-  uncompletedTodoItems(@Args() query: TodoItemQuery): Promise<ConnectionType<TodoItemDTO>> {
+  async uncompletedTodoItems(
+    @Args() query: TodoItemQuery,
+    @Context() context: UserContext,
+  ): Promise<ConnectionType<TodoItemDTO>> {
+    const authFilter = await this.authorizer.authorize(context);
     // add the completed filter the user provided filter
-    const filter: Filter<TodoItemDTO> = {
-      ...query.filter,
-      ...{ completed: { is: false } },
-    };
-
-    return TodoItemConnection.createFromPromise((q) => this.service.query(q), { ...query, ...{ filter } });
+    const filter: Filter<TodoItemDTO> = mergeFilter(query.filter ?? {}, { completed: { is: false } });
+    const uncompletedQuery = mergeQuery(query, { filter: mergeFilter(filter, authFilter) });
+    return TodoItemConnection.createFromPromise(
+      (q) => this.service.query(q),
+      uncompletedQuery,
+      (q) => this.service.count(q),
+    );
   }
 }
