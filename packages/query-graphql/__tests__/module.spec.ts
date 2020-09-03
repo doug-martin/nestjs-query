b4e68620a973caf4a6bc9ddc9947c0be7464fb11
doug-martin <doug@dougamartin.com>
2020-09-02 23:39:16 -0500

    fix(graphql, #505): Less restrictive readResolverOpts for auto crud

--- packages/query-graphql/__tests__/module.spec.ts
+++ packages/query-graphql/__tests__/module.spec.ts
@@ -24,4 +24,21 @@ describe('NestjsQueryGraphQLModule', () => {
     expect(graphqlModule.providers).toHaveLength(3);
     expect(graphqlModule.exports).toHaveLength(4);
   });
+
+  it('should allow a defaultFilter for read options', () => {
+    const graphqlModule = NestjsQueryGraphQLModule.forFeature({
+      imports: [],
+      resolvers: [
+        {
+          DTOClass: TestDTO,
+          EntityClass: TestDTO,
+          read: { defaultFilter: { name: { eq: 'foo' } } },
+        },
+      ],
+    });
+    expect(graphqlModule.imports).toHaveLength(1);
+    expect(graphqlModule.module).toBe(NestjsQueryGraphQLModule);
+    expect(graphqlModule.providers).toHaveLength(3);
+    expect(graphqlModule.exports).toHaveLength(4);
+  });
 });
