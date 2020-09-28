9f4e93b7726d85cb4febe86d2caf941dc957463a
JohnDoePlusPlus <alex.lozan@labs42.io>
2020-09-28 19:40:13 +0300

    fix(filters): Fix bug with incorect parameters in filters

--- packages/query-typeorm/src/query/sql-comparison.builder.ts
+++ packages/query-typeorm/src/query/sql-comparison.builder.ts
@@ -1,6 +1,6 @@
 import { CommonFieldComparisonBetweenType, FilterComparisonOperators } from '@nestjs-query/core';
 import { ObjectLiteral } from 'typeorm';
-
+import { v4 as uuid } from 'uuid';
 /**
  * @internal
  */
@@ -22,8 +22,6 @@ export type EntityComparisonField<Entity, F extends keyof Entity> =
  * Builder to create SQL Comparisons. (=, !=, \>, etc...)
  */
 export class SQLComparisonBuilder<Entity> {
-  paramCount = 0;
-
   static DEFAULT_COMPARISON_MAP: Record<string, string> = {
     eq: '=',
     neq: '!=',
@@ -40,8 +38,8 @@ export class SQLComparisonBuilder<Entity> {
   constructor(readonly comparisonMap: Record<string, string> = SQLComparisonBuilder.DEFAULT_COMPARISON_MAP) {}
 
   private get paramName(): string {
-    const param = `param${this.paramCount}`;
-    this.paramCount += 1;
+    const id = uuid().replace('-', '');
+    const param = `param${id}`;
     return param;
   }
 
