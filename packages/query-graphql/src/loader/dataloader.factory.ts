0ebafa893a536dbd2e815a636cb3989c4813ef4c
boxcc <lmfmcbo@gmail.com>
2020-10-20 22:34:38 +0800

    fix: dataloader cacheKeyFn bigint problem

--- packages/query-graphql/src/loader/dataloader.factory.ts
+++ packages/query-graphql/src/loader/dataloader.factory.ts
@@ -21,7 +21,10 @@ export class DataLoaderFactory {
     const nestjsQueryLoaders = this.initializeContext(context);
     if (!nestjsQueryLoaders[name]) {
       // eslint-disable-next-line no-param-reassign
-      nestjsQueryLoaders[name] = new Dataloader(handler, { cacheKeyFn: (key) => JSON.stringify(key) });
+      nestjsQueryLoaders[name] = new Dataloader(handler, {
+        cacheKeyFn: (key) =>
+          JSON.stringify(key, (_, v) => (typeof v === 'bigint' ? v.toString() : v)),
+      });
     }
     return nestjsQueryLoaders[name] as Dataloader<K, V>;
   }
