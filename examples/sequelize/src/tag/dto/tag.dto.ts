91771535395231eb34248e5783b44ea96dca20c4
doug-martin <doug@dougamartin.com>
2021-02-14 22:02:54 -0700

    example: Updated to use new decorators

--- examples/sequelize/src/tag/dto/tag.dto.ts
+++ examples/sequelize/src/tag/dto/tag.dto.ts
@@ -1,10 +1,10 @@
-import { FilterableField, FilterableConnection, KeySet } from '@nestjs-query/query-graphql';
+import { FilterableField, FilterableCursorConnection, KeySet } from '@nestjs-query/query-graphql';
 import { ObjectType, ID, GraphQLISODateTime } from '@nestjs/graphql';
 import { TodoItemDTO } from '../../todo-item/dto/todo-item.dto';
 
 @ObjectType('Tag')
 @KeySet(['id'])
-@FilterableConnection('todoItems', () => TodoItemDTO)
+@FilterableCursorConnection('todoItems', () => TodoItemDTO)
 export class TagDTO {
   @FilterableField(() => ID)
   id!: number;
