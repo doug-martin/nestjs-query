e9abcb741129bd73e77bbfd29e7b3d66d108cf54
doug-martin <doug@dougamartin.com>
2021-02-21 21:32:56 -0700

    example(hooks): Add injectable hooks example

---
+++ examples/hooks/src/sub-task/sub-task.module.ts
@@ -0,0 +1,25 @@
+import { NestjsQueryGraphQLModule } from '@nestjs-query/query-graphql';
+import { NestjsQueryTypeOrmModule } from '@nestjs-query/query-typeorm';
+import { Module } from '@nestjs/common';
+import { SubTaskDTO } from './dto/sub-task.dto';
+import { CreateSubTaskDTO } from './dto/subtask-input.dto';
+import { SubTaskUpdateDTO } from './dto/subtask-update.dto';
+import { SubTaskEntity } from './sub-task.entity';
+import { AuthModule } from '../auth/auth.module';
+
+@Module({
+  imports: [
+    NestjsQueryGraphQLModule.forFeature({
+      imports: [NestjsQueryTypeOrmModule.forFeature([SubTaskEntity]), AuthModule],
+      resolvers: [
+        {
+          DTOClass: SubTaskDTO,
+          EntityClass: SubTaskEntity,
+          CreateDTOClass: CreateSubTaskDTO,
+          UpdateDTOClass: SubTaskUpdateDTO,
+        },
+      ],
+    }),
+  ],
+})
+export class SubTaskModule {}
---
+++ examples/hooks/src/todo-item/dto/todo-item-input.dto.ts
@@ -0,0 +1,18 @@
+import { IsString, MaxLength, IsBoolean } from 'class-validator';
+import { Field, InputType } from '@nestjs/graphql';
+import { BeforeCreateMany, BeforeCreateOne } from '@nestjs-query/query-graphql';
+import { CreatedByHook } from '../../hooks';
+
+@InputType('TodoItemInput')
+@BeforeCreateOne(CreatedByHook)
+@BeforeCreateMany(CreatedByHook)
+export class TodoItemInputDTO {
+  @IsString()
+  @MaxLength(20)
+  @Field()
+  title!: string;
+
+  @IsBoolean()
+  @Field()
+  completed!: boolean;
+}
