d8a33d6a91877c307379b5b7b23c976615ee89d0
doug-martin <doug@dougamartin.com>
2021-02-21 21:32:27 -0700

    feat(graphql,hooks): Provide support for injectable hooks

---
+++ packages/query-graphql/src/hooks/types.ts
@@ -0,0 +1,10 @@
+export enum HookTypes {
+  BEFORE_CREATE_ONE = 'BeforeCreateOne',
+  BEFORE_CREATE_MANY = 'BeforeCreateMany',
+  BEFORE_UPDATE_ONE = 'BeforeUpdateOne',
+  BEFORE_UPDATE_MANY = 'BeforeUpdateMany',
+  BEFORE_DELETE_ONE = 'BeforeDeleteOne',
+  BEFORE_DELETE_MANY = 'BeforeDeleteMany',
+  BEFORE_QUERY_MANY = 'BeforeQueryMany',
+  BEFORE_FIND_ONE = 'BeforeFindOne',
+}
