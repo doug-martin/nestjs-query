ccc2abed3e31040c3c5cc958d35769f93536ccd1
doug-martin <doug@dougamartin.com>
2021-03-10 22:06:26 -0700

    chore(): Update typegoose example to use QueryOptions decorator

--- examples/typegoose/src/todo-item/todo-item.module.ts
+++ examples/typegoose/src/todo-item/todo-item.module.ts
@@ -22,7 +22,6 @@ const guards = [AuthGuard];
           AssemblerClass: TodoItemAssembler,
           CreateDTOClass: TodoItemInputDTO,
           UpdateDTOClass: TodoItemUpdateDTO,
-          enableTotalCount: true,
           enableAggregate: true,
           aggregate: { guards },
           create: { guards },
