ac5c12093d33a7678dd78a9bef9daa99b5815c57
Doug Martin <doug@dougamartin.com>
2021-03-17 18:59:36 -0600

    chore(graphql): remove graphql-relay depdency

--- packages/query-graphql/src/types/connection/cursor/pager/strategies/helpers.ts
+++ packages/query-graphql/src/types/connection/cursor/pager/strategies/helpers.ts
@@ -11,3 +11,11 @@ export function isForwardPaging(cursor: CursorPagingType): boolean {
 export function hasBeforeCursor(cursor: CursorPagingType): boolean {
   return isBackwardPaging(cursor) && !!cursor.before;
 }
+
+export function encodeBase64(str: string): string {
+  return Buffer.from(str, 'utf8').toString('base64');
+}
+
+export function decodeBase64(str: string): string {
+  return Buffer.from(str, 'base64').toString('utf8');
+}
