c63671e78d29dcedba3bfb7001b31388b808b822
Max Wölk <max@mwoelk.de>
2021-04-07 12:02:21 +0200

    refactor(graphql,auth,#1026): made context in authorizer not optional

--- packages/query-graphql/src/decorators/authorize-filter.decorator.ts
+++ packages/query-graphql/src/decorators/authorize-filter.decorator.ts
@@ -14,7 +14,7 @@ function getContext<C>(executionContext: ExecutionContext): C {
 
 function getAuthorizerFilter<C extends AuthorizerContext<unknown>>(
   context: C,
-  authorizationContext?: AuthorizationContext,
+  authorizationContext: AuthorizationContext,
 ) {
   if (!context.authorizer) {
     return undefined;
@@ -25,7 +25,7 @@ function getAuthorizerFilter<C extends AuthorizerContext<unknown>>(
 function getRelationAuthFilter<C extends AuthorizerContext<unknown>>(
   context: C,
   relationName: string,
-  authorizationContext?: AuthorizationContext,
+  authorizationContext: AuthorizationContext,
 ) {
   if (!context.authorizer) {
     return undefined;
@@ -36,8 +36,15 @@ function getRelationAuthFilter<C extends AuthorizerContext<unknown>>(
 function getAuthorizationContext(
   methodName: string | symbol,
   partialAuthContext?: PartialAuthorizationContext,
-): AuthorizationContext | undefined {
-  if (!partialAuthContext) return undefined;
+): AuthorizationContext {
+  if (!partialAuthContext)
+    return new Proxy<AuthorizationContext>({} as AuthorizationContext, {
+      get: () => {
+        throw new Error(
+          `No AuthorizationContext available for method ${methodName.toString()}! Make sure that you provide an AuthorizationContext to your custom methods as argument of the @AuthorizerFilter decorator.`,
+        );
+      },
+    });
 
   return {
     operationName: methodName.toString(),
