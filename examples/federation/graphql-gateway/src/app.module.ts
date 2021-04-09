46291c7ad509049caeb32bcf37220b95c26f1a5b
Max Wölk <max@mwoelk.de>
2021-04-09 09:27:56 +0200

    fix(graphql,federation,#1051): check for undefined as well

--- examples/federation/graphql-gateway/src/app.module.ts
+++ examples/federation/graphql-gateway/src/app.module.ts
@@ -13,6 +13,7 @@ import { GraphQLGatewayModule } from '@nestjs/graphql';
           { name: 'todo-items', url: 'http://localhost:3001/graphql' },
           { name: 'sub-tasks', url: 'http://localhost:3002/graphql' },
           { name: 'tags', url: 'http://localhost:3003/graphql' },
+          { name: 'user', url: 'http://localhost:3004/graphql' },
         ],
       },
     }),
