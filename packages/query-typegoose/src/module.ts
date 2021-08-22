45960f513d871c0990d8dab339f966c764037688
gimyboya <b.afatah@yahoo.com>
2021-08-22 14:51:28 +0800

    fix: importing types properly

--- packages/query-typegoose/src/module.ts
+++ packages/query-typegoose/src/module.ts
@@ -1,7 +1,8 @@
 import { DynamicModule } from '@nestjs/common';
-import { TypegooseModule, TypegooseClass, TypegooseClassWithOptions } from 'nestjs-typegoose';
+import { TypegooseModule } from 'nestjs-typegoose';
 import { Class } from '@nestjs-query/core';
 import { createTypegooseQueryServiceProviders } from './providers';
+import { TypegooseClassWithOptions, TypegooseClass } from 'nestjs-typegoose/dist/typegoose-class.interface';
 
 export class NestjsQueryTypegooseModule {
   static forFeature(models: (TypegooseClass | TypegooseClassWithOptions)[], connectionName?: string): DynamicModule {
