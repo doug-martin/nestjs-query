e3b91c120c3ce20200955912a83fb5f1b116b9d8
gimyboya <b.afatah@yahoo.com>
2021-08-22 14:43:13 +0800

    fix: allow discriminators to be used with typegoose

--- packages/query-typegoose/src/module.ts
+++ packages/query-typegoose/src/module.ts
@@ -1,10 +1,10 @@
 import { DynamicModule } from '@nestjs/common';
-import { TypegooseModule } from 'nestjs-typegoose';
+import { TypegooseModule, TypegooseClass, TypegooseClassWithOptions } from 'nestjs-typegoose';
 import { Class } from '@nestjs-query/core';
 import { createTypegooseQueryServiceProviders } from './providers';
 
 export class NestjsQueryTypegooseModule {
-  static forFeature(models: Class<unknown>[], connectionName?: string): DynamicModule {
+  static forFeature(models: (TypegooseClass | TypegooseClassWithOptions)[], connectionName?: string): DynamicModule {
     const queryServiceProviders = createTypegooseQueryServiceProviders(models);
     const typegooseModule = TypegooseModule.forFeature(models, connectionName);
     return {
