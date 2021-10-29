d328d2beb8c0ebc3048631a97e5b2023b1891b25
Tycho Bokdam <tycho@palmtreecoding.com>
2021-10-29 11:44:16 +0200

    fix(core): Use correct return types for decorators

--- packages/core/src/decorators/inject-query-service.decorator.ts
+++ packages/core/src/decorators/inject-query-service.decorator.ts
@@ -2,5 +2,5 @@ import { Inject } from '@nestjs/common';
 import { Class } from '../common';
 import { getQueryServiceToken } from './helpers';
 
-export const InjectQueryService = <DTO>(DTOClass: Class<DTO>): ParameterDecorator =>
+export const InjectQueryService = <DTO>(DTOClass: Class<DTO>): ReturnType<typeof Inject> =>
   Inject(getQueryServiceToken(DTOClass));
