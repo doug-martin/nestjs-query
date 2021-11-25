5c0a35a252971f9c2f686bcd99eda09646a60856
Doug Martin <doug@dougamartin.com>
2021-11-24 20:48:02 -0700

    chore(deps): update linting packages

--- packages/query-graphql/__tests__/types/cursor.scalar.spec.ts
+++ packages/query-graphql/__tests__/types/cursor.scalar.spec.ts
@@ -4,19 +4,19 @@ import { ConnectionCursorScalar } from '../../src';
 describe('ConnectionCursorScalar', (): void => {
   describe('#parseValue', () => {
     it('should parse a value', () => {
-      expect(ConnectionCursorScalar.parseValue('aaa')).toEqual('aaa');
+      expect(ConnectionCursorScalar.parseValue('aaa')).toBe('aaa');
     });
   });
 
   describe('#serialize', () => {
     it('should serialize a value', () => {
-      expect(ConnectionCursorScalar.serialize('aaa')).toEqual('aaa');
+      expect(ConnectionCursorScalar.serialize('aaa')).toBe('aaa');
     });
   });
 
   describe('#parseLiteral', () => {
     it('should parse a literal', () => {
-      expect(ConnectionCursorScalar.parseLiteral({ kind: Kind.STRING, value: 'aaa' }, {})).toEqual('aaa');
+      expect(ConnectionCursorScalar.parseLiteral({ kind: Kind.STRING, value: 'aaa' }, {})).toBe('aaa');
     });
 
     it('should return null if the ast.kind is not a string', () => {
--- packages/query-graphql/src/types/validators/is-undefined.validator.ts
+++ packages/query-graphql/src/types/validators/is-undefined.validator.ts
@@ -2,7 +2,7 @@ import { ValidationOptions, ValidateIf } from 'class-validator';
 
 /** @internal */
 export function IsUndefined(validationOptions?: ValidationOptions) {
-  // eslint-disable-next-line @typescript-eslint/no-explicit-any,@typescript-eslint/explicit-module-boundary-types
-  return (obj: any, property: string) =>
+  // eslint-disable-next-line @typescript-eslint/ban-types
+  return (obj: Object, property: string) =>
     ValidateIf((o: Record<string, unknown>) => o[property] !== undefined, validationOptions)(obj, property);
 }
