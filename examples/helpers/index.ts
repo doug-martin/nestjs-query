a27dced5138b6cdda59a977a116abcdfb60e11f9
Zachary Volpi <hello@zvolpi.dev>
2022-04-18 01:28:56 +0200

    chore(debs): remove other mongoose ref

--- examples/helpers/index.ts
+++ examples/helpers/index.ts
@@ -1,4 +1,3 @@
 export * from './db-test.helpers';
 export * from './typeorm.helpers';
 export * from './sequelize.helpers';
-export * from './mongoose.helpers';
--- examples/helpers/mongoose.helpers.ts
+++
@@ -1,7 +0,0 @@
-import { MongooseModuleOptions } from '@nestjs/mongoose';
-
-export const mongooseConfig = (db: string, overrides?: Partial<MongooseModuleOptions>): MongooseModuleOptions => ({
-  uri: `mongodb://localhost/${db}`,
-  useNewUrlParser: true,
-  ...overrides,
-});
