86f1454344f4cb7a10929476ac03a8ae25439487
Zachary Volpi <hello@zvolpi.dev>
2022-04-18 01:17:17 +0200

    chore(library): remove testing bed for mongoose & typegoose

--- examples/mongoose/src/helpers.ts
+++
@@ -1,4 +0,0 @@
-import { USER_HEADER_NAME } from './constants';
-import { GqlContext } from './auth.guard';
-
-export const getUserName = (context: GqlContext): string => context.request.headers[USER_HEADER_NAME];
